235484db701922b94f0794990caa9d68e98efd1f
Fauzan <mohammad.fauzan21@ui.ac.id>
2025-01-21 23:13:35 +0700

    feat: UI Contact Us done

--- src/contact-us/screen/Contact.jsx
+++ src/contact-us/screen/Contact.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import FormControl from '../component/FormControl'
+import ButtonCustom from '/src/practice-test/component/ButtonCustom'
 
 const Contact = () => {
   return (
     <section className="contact">
       <div className="content">
-        Contact
+        <h1 className="header-text">Contact Us</h1>
+        <div className="core-content">
+          <h1>Have concerns? Let us know, and we’ll be happy to help.</h1>
+          <div className="box-form">
+            <FormControl />
+            <ButtonCustom name="Send" />
+          </div>
+        </div>
       </div>
     </section>
   )
